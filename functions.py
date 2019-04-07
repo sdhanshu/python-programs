ff8d771f44ca973324ad06ce9647402e818403fc
ricksanc <sudhanshuchoudhary2005@gmail.com>
2019-04-07 16:15:55 +0530

    Added functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+# A function is a collection of code
+
+def function1():
+    print("\nThis is the first function: ")
+    print("\nHello! :-)")
+
+function1()         # Called / Used a functions
+
+# This is called a Mapping:
+
+def function2(x):   # Here x is an Argument
+    return x*2
+print("\nThis is the second function: \n")
+
+function2(3)        # It will not work by calling
+                    # 3 is the value of x
+print(function2(3)) # We have to print it out
